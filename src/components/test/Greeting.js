7a81f2c0b079524d8158d75b56c8dc68d1053131
Punamkumari03 <punamgupta250602@gmail.com>
2023-08-10 17:16:57 +0530

    test user interface

--- src/components/test/Greeting.js
+++ src/components/test/Greeting.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Greeting = () => {
+const[changedtext,setChangedText] = useState(false)
+const changeTextHandler =() =>{
+  setChangedText(true)
+}
   return (
     <div>
       <h2>Hello World</h2>
+     {!changedtext && <p>It's good to see you</p>}
+     {changedtext && <p>changed!</p>}
+      <button onClick={changeTextHandler}>Change text</button>
     </div>
   )
 }
--- src/components/test/Greeting.test.js
+++ src/components/test/Greeting.test.js
@@ -1,16 +1,30 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
-
-test('renders Hello World as a test ',()=>{
-   //Arrange
-    render(<Greeting/>);
-
-    //Act
-    //..nothing
-
-    //Assert
-
-    const helloWorldElement = screen.getByText('Hello World');
-    expect(helloWorldElement).toBeInTheDocument();
-
-});
+import userEvent from "@testing-library/user-event";
+describe('Greeting Component',()=>{
+    test('renders Hello World as a test ',()=>{
+        //Arrange
+         render(<Greeting/>);
+     
+         //Act
+         //..nothing
+     
+         //Assert
+     
+         const helloWorldElement = screen.getByText('Hello World');
+         expect(helloWorldElement).toBeInTheDocument();
+     
+     });
+     test('render good to see you if the button was not clicked',()=>{
+        render(<Greeting/>)
+        const OutputElement = screen.getByText('good to see you',{exact:false});
+        expect(OutputElement).toBeInTheDocument();
+     })
+    //  test('render Changed if the button was clicked',()=>{
+    //     render(<Greeting/>)
+    //     const buttonElement = screen.getByRole('button');
+    //     userEvent.click(buttonElement)
+    //     const OutputButton = screen.getByText('changed!');
+    //     expect(OutputButton).toBeInTheDocument();
+    //  })
+})
